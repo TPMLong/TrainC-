a29a5297714b373985fc8a5842ba03f595a78820
TPMLong <trieuminhlong2000@gmail.com>
2021-02-27 18:39:29 +0700

    first add v0.1

---
+++ Week1Assign/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Week1Assign.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) {}
+    }
+}
---
+++ Week1Assign/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Week1Assign.Models
+{
+    public class AppUser : IdentityUser
+    {
+    }
+}
